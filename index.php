f0c7acfb3738c82cbf41e69501fb50a1cc62647e
lpitto86 <luca.p481@gmail.com>
2024-12-19 17:50:04 +0100

    Eliminated word

--- index.php
+++ index.php
@@ -2,11 +2,17 @@
     // Controlla se i campi sono definiti
     $originalParagraph = isset($_GET['paragraph']) ? $_GET['paragraph'] : '';
     $badword = isset($_GET['badword']) ? $_GET['badword'] : '';
+    $removeword = isset($_GET['removeword']) ? $_GET['removeword'] : '';
     $censoredParagraph = $originalParagraph;
 
     // Sostituisce la parola solo se fornita
     if ($badword !== '') {
-        $censoredParagraph = str_replace($badword, '***', $originalParagraph);
+        $censoredParagraph = str_replace($badword, '*****', $originalParagraph);
+    }
+
+    // Rimuove la parola solo se fornita
+    if ($removeword !== '') {
+        $censoredParagraph = str_replace($removeword, '', $censoredParagraph);
     }
 ?>
 
@@ -16,6 +22,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Censorship</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
     </head>
     <body>
         <header>
@@ -38,6 +45,12 @@
                     <input type="text" name="badword" id="badword" placeholder="Inserisci parola da censurare...">
                 </div>
                 <div>
+                    <div>
+                        <label for="removeword">Parola da eliminare</label>
+                    </div>
+                    <input type="text" name="removeword" id="removeword" placeholder="Inserisci parola da eliminare...">
+                </div>
+                <div>
                     <button type="submit">
                         Invia
                     </button>
@@ -58,7 +71,18 @@
                         <h3>Parola censurata: <?php echo $badword; ?></h3>
                     </div>
                 <?php endif; ?>
+                <?php if ($removeword !== ''): ?>
+                    <div>
+                        <h3>Parola eliminata: <?php echo $removeword; ?></h3>
+                    </div>
+                <?php endif; ?>
             <?php endif; ?>
         </main>
+        <script>
+            // Resetta i campi del form dopo il refresh
+            document.getElementById('paragraph').value = "";
+            document.getElementById('badword').value = "";
+            document.getElementById('removeword').value = "";
+        </script>
     </body>
 </html>
